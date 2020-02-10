73d7065ab6b60669c4e801b7aa3842a80335f522
Mike Sadowski <msadowsk@opentext.com>
2020-02-10 16:05:24 -0500

    Scan file uploaded to endpoint

--- routes/scan.js
+++ routes/scan.js
@@ -1,9 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const fs = require('fs');
+
+const exec = require('child_process').exec;
 
 router.post('/file', (req, res, next) => {
-    console.log('Scanning file ...');
-    res.send('Finished Scanning');
+    if (!req.files) {
+        return res.status(400).send('No files were scanned')
+    }
+
+    console.log('===> Scanning file ...');
+
+    const file = req.files.file;
+
+    exec('clamscan' + ' ' + file, (error, stdout, stderr) => {
+        const lines = stdout.toString().split('\n')[0].split(' ')[1];
+        const virus = stdout.toString().split('\n')[7].split(' ')[2];
+
+        res.send('Scan complete');
+    });
 });
 
 module.exports = router;
